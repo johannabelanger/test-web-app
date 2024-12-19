1ff0682c4511452842476f78c9848841843b0069
Johanna Belanger <johanna.belanger@gmail.com>
2024-12-19 15:00:03 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [serverMessage, setServerMessage] = useState('Not ready yet. Wait for it...')
+
+  const getServerMessage = () => {
+    axios.get('/api/customers')
+    .then((response) => setServerMessage(response.data))
+    .catch((error) => setServerMessage(error.message));
+  };
+
+  useEffect(() => getServerMessage(), []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <p>Message from API is : {serverMessage}</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
